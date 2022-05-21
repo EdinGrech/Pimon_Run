28087f3335e0fce44c8055699f83091bfda4d1fa
EdinGrech <edin.grech@gmail.com>
2022-05-21 18:57:11 +0200

    missing path

--- AutoUpdateLib_/autoUpdate.py
+++ AutoUpdateLib_/autoUpdate.py
@@ -1,14 +1,14 @@
-import AutoUpdateLib as AutoUpdate
+import AutoUpdateLib_.AutoUpdateLib as AutoUpdateLib
 import subprocess
 import sys
 import pathlib
-path = pathlib.Path().resolve()
+path = str(f"{pathlib.Path().resolve()}/Pimon_Run")
 url_mothership = "https://raw.githubusercontent.com/EdinGrech/Pimon_Run/master/"
-AutoUpdate.set_url("https://raw.githubusercontent.com/EdinGrech/Pimon_Run/master/version.txt")
+AutoUpdateLib.set_url("https://raw.githubusercontent.com/EdinGrech/Pimon_Run/master/version.txt")
 
 def get_update_file_list():
     update_list = [] #"file names"
-    with open("updateFiles.txt", "r") as f:
+    with open(f"{path}/updateFiles.txt", "r") as f:
         for line in f:
             update_list.append(line)
         f.close()
@@ -16,26 +16,25 @@ def get_update_file_list():
         
 #read current verson from local file version.txt
 def get_current_version():
-    with open(str(f"{path}\\version.txt"), "r") as f:
-        AutoUpdate.set_current_version(f.read())
+    with open(str(f"{path}/version.txt"), "r") as f:
+        AutoUpdateLib.set_current_version(f.read())
         f.close()
 
 def file_updater(update_list):
     for file in update_list:
-        url_file = file.replace('\\', '/')
-        AutoUpdate.set_download_link(str(f"{url_mothership}{url_file}")) #set the download link
-        AutoUpdate.download(str(f"{path}\\{file}")) #if the version is up to date we just start the program
+        AutoUpdateLib.set_download_link(str(f"{url_mothership}{file}")) #set the download link
+        AutoUpdateLib.download(str(f"{path}/{file}")) #if the version is up to date we just start the program
 
 def update_sequence():
     get_current_version()
-    if not AutoUpdate.is_up_to_date():
+    if not AutoUpdateLib.is_up_to_date():
         file_updater(["updateFiles.txt"])#fisrt we update the list of things that need to be updated
         file_updater(get_update_file_list())#then we update the files
-        with open(str(f"{path}\\version.txt"), "w") as f: #and finally we update the version file
-            f.write(AutoUpdate.get_latest_version())
+        with open(str(f"{path}/version.txt"), "w") as f: #and finally we update the version file
+            f.write(AutoUpdateLib.get_latest_version())
             f.close()
         print("Update complete")
-        subprocess.Popen(str(f"{path}\\PiMon_Main.py")) #and we start the new version
+        subprocess.Popen(str(f"{path}/PiMon_Main.py")) #and we start the new version
         sys.exit(exit_code=0)
 
         
